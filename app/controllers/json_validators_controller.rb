64535a9547308d138630a099bfaf9f147d2dbe7b
robin-hassan <robiul.hassan12102@gmail.com>
2024-06-09 13:01:45 -0400

    feat: reafactor ux for string generator

--- app/controllers/json_validators_controller.rb
+++ app/controllers/json_validators_controller.rb
@@ -1,6 +1,35 @@
 class JsonValidatorsController < ApplicationController
   def index
-    @data = ""
+    @data = [
+      {
+        color: "red",
+        value: "#f00"
+      },
+      {
+        color: "green",
+        value: "#0f0"
+      },
+      {
+        color: "blue",
+        value: "#00f"
+      },
+      {
+        color: "cyan",
+        value: "#0ff"
+      },
+      {
+        color: "magenta",
+        value: "#f0f"
+      },
+      {
+        color: "yellow",
+        value: "#ff0"
+      },
+      {
+        color: "black",
+        value: "#000"
+      }
+    ]
   end
 
   def create
