e5f6a4afa659dd1bfc6f82fc6c6eb9c3119a926e
robin-hassan <robiul.hassan12102@gmail.com>
2024-06-09 13:26:24 -0400

    feat: make copy button

--- app/controllers/string_generators_controller.rb
+++ app/controllers/string_generators_controller.rb
@@ -3,6 +3,24 @@ class StringGeneratorsController < ApplicationController
   end
 
   def create
+    puts "---------#{validators_params}"
     respond_to { |format| format.turbo_stream }
   end
+
+  def validators_params
+    params.require(:string_generators).permit(:category,
+                                              :include_numbers,
+                                              :include_lowercase,
+                                              :include_uppercase,
+                                              :begin_with_letter,
+                                              :include_symbols,
+                                              :no_similar_characters,
+                                              :no_duplicate_characters,
+                                              :no_seq_characters,
+                                              :auto_generate_first_call,
+                                              :quantity,
+                                              :save_my_preference
+    )
+  end
+
 end
